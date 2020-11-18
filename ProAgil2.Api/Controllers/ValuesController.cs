ee17b7842f7b17d3a116cbb550f0eb87de35d576
Guilherme Rodrigues <guilhermejonathan@hotmail.com>
2020-11-18 19:20:03 -0300

    Add project files.

---
+++ ProAgil2.Api/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAgil2.Api.Data;
+using ProAgil2.Api.Model;
+
+namespace ProAgil2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Evento>> Get()
+        {
+
+            var eventos = _context.Eventos.ToList();
+            return eventos;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Evento> Get(int id)
+        {
+            var eventos = _context.Eventos.FirstOrDefault(a => a.EventoId == id);
+            return eventos;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
